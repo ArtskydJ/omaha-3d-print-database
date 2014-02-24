20b32db65e2805a669ddfcc9f1c1b919611d8d14
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-24 16:39:00 -0600

    Added comments to index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+
+//write a javascript module to write to sql database
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'example.org',
+	user     : 'root',
+	password : 'secret'
+});
+
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
+});
+
 var insert = function insert(hash, obj) {
+	//insert function (hash, obj, cb)
+	//(adds a row by running and "insert" query)
 	
 }
 
 var get = function get(connection, cb) {
+	//get function (hash, cb) (runs a select query)
 	connection.query("SELECT ", cb) //Fix this string to do the stuff we want.
 }
 
@@ -10,3 +27,4 @@ module.exports = {
 	insert: insert,
 	get: get
 }
+
