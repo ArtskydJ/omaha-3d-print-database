6c6ff400c77b8de6134028f843ff9a14bdee8844
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-03 18:08:13 -0600

    Added nameOfTable var and INSERT query

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ var connection = mysql.createConnection({
 	user     : 'root',
 	password : 'secret'
 })
+var nameOfTable = "admeshObjs"
 
 var insert = function insert(hash, obj, cb) {
 	//(adds a row by running and "insert" query)
-	
+	connection.query(
+		"INSERT INTO " + nameOfTable + "\nVALUES (" + hash + obj.all.toString() + ");",
+	cb)	
 }
 
 var get = function get(hash, cb) {
 	//get function (hash, cb) (runs a select query)
-	connection.query("SELECT ", cb) //Fix this string to do the stuff we want.
+	connection.query("SELECT * FROM "+nameOfTable, cb) //Fix this string to do the stuff we want.
 }
 
 module.exports = {
