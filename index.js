8c178e68d6b2e0620bcb9b3887cc10e8437c778a
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-13 21:31:10 -0500

    copied names from create.sql, added 'remove' func, deleted 'all' func

--- index.js
+++ index.js
@@ -3,25 +3,23 @@
 var mysql = require('mysql');
 
 var insert = function insert(conn, hash, obj, cb) { //adds a row by running and "insert" query)
-	conn.query("INSERT INTO admeshtable (id, hash, volume, parts, minX, maxX, "+
-		"minY, maxY, minZ, maxZ) VALUES ('?', '?', '?', '?', '?', '?', '?', "+
-		"'?', '?', '?')".escape(obj.id, hash, obj.volume, obj.parts,
-		obj.x.min, obj.x.max, obj.y.min, obj.y.max, obj.z.min, obj.z.max), cb)
-}
-
-var all = function all(conn, cb) { //get all (hash, cb) (SELECT)
-	console.log("index.all(" + conn + ", " + cb + ")")
-	conn.query("SELECT * FROM admeshtable", cb)
+	conn.query("INSERT INTO stl_properties (hash, volume, parts, minX, maxX, minY, maxY, minZ, maxZ) " +
+		"VALUES (?, '?', '?', '?', '?', '?', '?', '?', '?')", [hash, obj.volume,
+		obj.parts, obj.x.min, obj.x.max, obj.y.min, obj.y.max, obj.z.min, obj.z.max], cb)
 }
 
 var get = function get(conn, hash, cb) { //get a row (hash, cb) (SELECT)
-	conn.query("SELECT * FROM admeshtable WHERE hash = ?".escape(hash), cb)
+	conn.query("SELECT * FROM stl_properties WHERE hash = ?", [hash], cb)
+}
+
+var remove = function remove(conn, hash, cb) { //get a row (hash, cb) (SELECT)
+	conn.query("DELETE FROM stl_properties WHERE `hash` = ?;", [hash], cb)
 }
 
 module.exports = function(conn) {
 	return {
 		insert: insert.bind(null, conn),
-		all: all.bind(null, conn),
-		get: get.bind(null, conn)
+		get:    get.bind(null, conn),
+		remove: remove.bind(null, conn)
 	}
 }
