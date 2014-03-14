3dd72e538017e70c9242dea2609fb16eb433a6f4
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-13 21:32:12 -0500

    updated test; uses new names, fixes bugs, etc

--- test/testDatabase.js
+++ test/testDatabase.js
@@ -6,98 +6,59 @@ var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : mysqlPassword,
-	database : "newdatabase20140219" //database, not table
-	//database : "conn20140313" //database, not table
+	database : "omaha3dprint"
 })
+var safeErrMessage = require("../../safe-error-message/index.js")
 var index = new Index(connection)
-var createSql = require("../create-4.js")
-
 var fakeHash = "ba4301c9e5aa93d96bdb5c87d9cf089d"
 
-var insertObject = {
-	x: { min: -1.334557, max: 1.370952 },
-	y: { min: -1.377953, max: 1.37723 },
-	z: { min: -1.373225, max: 1.242838 },
-	facets: {
-		overall:       { before: 3656, after: 3656 },
-		disconnected1: { before: 18,   after: 0 },
-		disconnected2: { before: 3,    after: 0 },
-		disconnected3: { before: 0,    after: 0 },
-		disconnected:  { before: 21,   after: 0 },
-		degenerate: 4,
-		removed: 14,
-		added: 3,
-		reversed: 2
-	},
-	edges: { fixed: 24, backwards: 0 },
-	volume: 10.889216,
-	parts: 1,
-	normalsFixed: 12,
-	all: [
-		-1.334557,	1.370952,	-1.377953,	1.37723,
-		-1.373225,	1.242838,	3656,		3656,
-		1,	18,	0,	2,	3,	0,	3,	0,	0,	21,	0,
-		1,	10.889216,	4,	24,	14,	3,	2,	0,	12
-	]
+var insertObject = { //stores only 4 numerals after the '.'
+	x: { min: -1.3345, max: 1.3709 },
+	y: { min: -1.3779, max: 1.3772 },
+	z: { min: -1.3732, max: 1.2428 },
+	volume: 10.8892,
+	parts: 1
+}
+var expectedObject = {
+	id: 0, //this gets changed later
+	hash: fakeHash,
+	volume: insertObject.volume,
+	parts: insertObject.parts,
+	minX: insertObject.x.min, maxX: insertObject.x.max,
+	minY: insertObject.y.min, maxY: insertObject.y.max,
+	minZ: insertObject.z.min, maxZ: insertObject.z.max
 }
 
 
 test("insert descriptive description here!", function(t) {
-	t.plan(6)
-	console.log("testing")
+	t.plan(11)
 	
 	t.equal(typeof index.insert, "function", "Has a function called 'insert'")
 	t.equal(typeof index.get, "function", "Has a function called 'get'")
 	
-	console.log("createSql: "+createSql)
-	
 	connection.connect(function (err) {
-		t.notOk(err)
-		if (err) {
-			console.log("heres the error: "+err.message)
-		} else {
-			console.log("yay no error")
-			connection.query(createSql, function(err) {
-				console.log("create: "+err.message||"lolz")
-				if (err) {
-					console.log("oh noes!!! - 0")
-					throw err
-				} else {
-					console.
-					index.all(connection, function(err, data) {
-						if (err) {
-							throw err
-						} else {
-							console.log("data: " + data)
-						}
-					})
-					index.insert(connection, fakeHash, insertObject, function(err) {
-						console.log("insert")
-						if (err) {
-							console.log("oh noes!!! - 1")
-							throw err
-						} else {
-							console.log("no err so far...")
-							index.get(connection, fakeHash, function(err, data) {
-								console.log("get")
-								if (err) {
-									throw err
-								} else {
-									t.equal(typeof data, "object", "returned data is an obj")
-									t.equal(data, insertObject, "returned data is the expected data")
-									
-									index.insert(connection, fakeHash, insertObject, function(err) {
-										t.ok(err, "does not allow same hash 2x")
-									})
-								}
-							})
-						}
-					})
-				}
+		t.notOk(err, "connection error" + safeErrMessage(err))
+		index.remove(fakeHash, function(err) {
+			t.notOk(err, "'remove' error" + safeErrMessage(err))
+			index.insert(fakeHash, insertObject, function(err) {
+				t.notOk(err, "'insert' error" + safeErrMessage(err))
+				index.get(fakeHash, function(err, data) {
+					t.notOk(err, "'get' error" + safeErrMessage(err))
+					t.ok(data, "data is truthy (not null)")
+					t.equal(typeof data, "object", "returned data is an obj")
+					t.equal(data.length, 1, "only returned one object")
+					console.log("\n\n\n\n\n\nDATA:\n")
+					console.dir(data[0])
+					console.log("\n\n\n\n\n\n")
+					expectedObject.id = data[0].id
+					t.similar(data[0], expectedObject, "returned data is the expected data")
+				})
+				index.insert(fakeHash, insertObject, function(err) {
+					t.ok(err, "does not allow same hash 2x") //if objs are truthy, then this is buggy
+				})
 			})
-		}
+		})
 	})
-	console.log("testing 2")
 	
 	setTimeout(function() {
 		t.end()
